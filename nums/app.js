d1adb3109f490aab04b6278bbc848d80b4becb8b
Zach Bunting <zvrbunting@gmail.com>
2024-11-20 11:55:40 -0800

    Finished q1

--- nums/app.js
+++ nums/app.js
@@ -1,8 +1,16 @@
 const BASE_URL = "http://numbersapi.com";
 
 // 1.
+// make request to Numbers API
+// get back JSON
+
 
 async function showNumberTrivia(num) {
+  const resp = await fetch(`${BASE_URL}/${num}?json`);
+  const data = await resp.json();
+
+  console.log('Favorite number is: ', num);
+  console.log(data.text);
 }
 
 // 2.
@@ -22,6 +30,7 @@ async function showNumberAll(nums) {
 /** Call all three functions one-after-another. */
 
 async function main() {
+  await showNumberTrivia(25);
 }
 
 await main();
